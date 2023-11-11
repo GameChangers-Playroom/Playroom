0fb4ceb41e812a2e4a7778d7b95c3e2ac3cd0f7a
Awakened-Redstone <40528665+Awakened-Redstone@users.noreply.github.com>
2023-11-11 17:58:40 -0300

    chore: Disable regen when holding the gun or frozen

---
+++ src/main/java/io/github/flameyheart/playroom/mixin/PlayerEntityAccessor.java
@@ -0,0 +1,24 @@
+package io.github.flameyheart.playroom.mixin;
+
+import io.github.flameyheart.playroom.duck.ExpandedEntityData;
+import io.github.flameyheart.playroom.item.LaserGun;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.world.GameRules;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PlayerEntity.class)
+public interface PlayerEntityAccessor {
+    @Accessor ItemStack getSelectedItem();
+}
--- src/main/java/io/github/flameyheart/playroom/mixin/PlayerEntityMixin.java
+++ src/main/java/io/github/flameyheart/playroom/mixin/PlayerEntityMixin.java
@@ -1,20 +1,26 @@
 package io.github.flameyheart.playroom.mixin;
 
 import io.github.flameyheart.playroom.duck.ExpandedEntityData;
+import io.github.flameyheart.playroom.item.LaserGun;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.world.GameRules;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixin extends LivingEntity {
+    @Shadow private ItemStack selectedItem;
+
     protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
         super(entityType, world);
     }
@@ -40,6 +46,14 @@ public abstract class PlayerEntityMixin extends LivingEntity {
             return damageMultiplier * 0.3f; //TODO: config
         }
         return damageMultiplier;
+    }
+
+    @Redirect(method = "tickMovement", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/GameRules;getBoolean(Lnet/minecraft/world/GameRules$Key;)Z"))
+    private boolean disableRegen(GameRules instance, GameRules.Key<GameRules.BooleanRule> rule) {
+        if (((ExpandedEntityData) this).playroom$isFrozen() || this.selectedItem.getItem() instanceof LaserGun) {
+            return false; //TODO: config
+        }
+        return instance.getBoolean(rule);
     }
 
 }
