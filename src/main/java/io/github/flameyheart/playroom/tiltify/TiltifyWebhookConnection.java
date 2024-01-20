d92a705b79ec23bd32401eabcd69ef0093c544d0
Awakened-Redstone <40528665+Awakened-Redstone@users.noreply.github.com>
2024-01-20 15:09:55 -0300

    chore: ;
    Signed-off-by: Awakened-Redstone <40528665+Awakened-Redstone@users.noreply.github.com>

--- src/main/java/io/github/flameyheart/playroom/tiltify/TiltifyWebhookConnection.java
+++ src/main/java/io/github/flameyheart/playroom/tiltify/TiltifyWebhookConnection.java
@@ -383,7 +383,7 @@ public class TiltifyWebhookConnection extends Thread {
 
         if (minDistance <= threshold) {
             return processAlternative(closestMatch);
-        };
+        }
 
         return name;
     }
